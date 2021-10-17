3bd42ec5aacf3ba28adda90dc65d87aa82f0df5f
unknown <ericsshyus@gmail.com>
2021-10-17 14:38:33 -0400

    redux persist on cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,22 +1,23 @@
 import { combineReducers } from "redux";
 // import userReducer from "./user/user.reducer";
 // import cartReducer from "./cart/cart.reducer";
-//import { persistReducer } from "redux-persist";
-//import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 //import directoryReducer from "./directory/directory.reducer";
 import shopReducer from "./shop/shop.reducer";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
 //persist config is used for local storage
-// const persistConfig = {
-//   key: "root",
+const persistConfig = {
+  key: "root",
+  storage,
 
-//   whitelist: ["cart"],
-// };
-export default combineReducers({
+  whitelist: ["cart"],
+};
+const rootReducer = combineReducers({
   cart: cartReducer,
   shop: shopReducer,
   user: userReducer,
 });
 
-//export default persistReducer(persistConfig, rootReducer);
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
-//import { persistStore } from "redux-persist";
+import { persistStore } from "redux-persist";
 //import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
 
@@ -15,6 +15,6 @@ const middlewares = [logger, sagaMiddleware];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
 
-//export const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-//export default { store };
+export default { store, persistor };
